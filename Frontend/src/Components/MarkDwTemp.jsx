255c0c4b7400a1f55f54d5ffabaf806db0332c1e
Aditya <saditya2094@gmail.com>
2024-12-14 20:41:31 +0530

    markdown edit

--- Frontend/src/Components/MarkDwTemp.jsx
+++ Frontend/src/Components/MarkDwTemp.jsx
@@ -4,9 +4,9 @@ import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 export const MarkDwTemp = ({markedContent}) => {
-  // console.log(markedContent)
+  console.log(markedContent)
 const htmlmarked = marked(markedContent);
-// console.log(htmlmarked);
+console.log(htmlmarked);
   return (
 
     <div className="prose prose-lg">
